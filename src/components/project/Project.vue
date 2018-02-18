c417417e2666dd418fe7aae1cf6131a29c280b1f
Li Kai <falconets25601@gmail.com>
2018-02-18 21:48:48 +0800

    Add project page

---
+++ src/components/project/Project.vue
@@ -0,0 +1,90 @@
+<template>
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="18">
+    <el-row :gutter="20">
+      <el-col :xs="24" :sm="16">
+        <img src="http://via.placeholder.com/150x150" class="image">
+      </el-col>
+      <el-col :xs="24" :sm="8">
+        <h1>{{project.title}}</h1>
+        <h2>{{project.description}}</h2>
+        <el-progress :percentage="fundingPercentage" :status="fundingStatus"></el-progress>
+        <el-row class="keywords">
+          <el-col :span="1" v-for="(keyword, index) in project.keywords" :key="index">
+            <el-tag type="info">{{keyword}}</el-tag>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <p>
+          Started on {{ new Date(project.startDate).toLocaleString() }}
+        </p>
+        <p>
+          Closes in {{ timeLeft }}
+        </p>
+        <el-slider :max="1000" v-model="fundingAmount" show-input></el-slider>
+        <el-button type="primary" @click="submit()">
+          Back project
+        </el-button>
+      </el-col>
+    </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import projects from '../../fixtures';
+export default {
+  name: 'project',
+  data() {
+    return {
+      project: projects[this.$route.params.id],
+      fundingAmount: 0,
+    };
+  },
+  computed: {
+    timeLeft() {
+      const secondsLeft =
+        Date.now() - this.project.startDate + this.project.duration;
+      const minutesLeft = secondsLeft / 60;
+      if (minutesLeft < 60) return `${Math.round(minutesLeft)} minutes`;
+      const hoursLeft = minutesLeft / 60;
+      if (hoursLeft < 24) return `${Math.round(hoursLeft)} hours`;
+      const daysLeft = hoursLeft / 24;
+      return `${Math.round(daysLeft)} days`;
+    },
+    fundingPercentage() {
+      const { fundingRaised, fundingRequired } = this.project;
+      const twoDecimalPlaces = (fundingRaised / fundingRequired * 100).toFixed(2);
+      return parseFloat(twoDecimalPlaces);
+    },
+    fundingStatus() {
+      const secondsLeft =
+        Date.now() - this.project.startDate + this.project.duration;
+      if (secondsLeft < 0) return 'exception';
+      return this.fundingPercentage >= 100 ? 'success' : '';
+    },
+  },
+  methods: {
+    submit() {
+      this.$notify({
+        title: 'Success!',
+        message: "You've backed a project",
+        type: 'success',
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.image {
+  width: 100%;
+  object-fit: cover;
+}
+.keywords {
+  margin: 0.5rem 0.5rem 0 0;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Root from "../components/Root.vue";
+import Project from "../components/project/Project.vue";
 import NewProject from "../components/project/New.vue";
 
 Vue.use(Router);
@@ -16,6 +17,11 @@ export default new Router({
     {
       path: "/projects/new",
       component: NewProject
-    }
+    },
+    {
+      path: '/projects/:id',
+      component: Project,
+      name: 'projects'
+    },
   ]
 });
