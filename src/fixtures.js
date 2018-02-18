176fe7b2cf88da15e40e44f3fde923b97ca906e1
Li Kai <falconets25601@gmail.com>
2018-02-18 21:39:22 +0800

    Add fixtures file

---
+++ src/fixtures.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    id: 0,
+    title: "Lorem ipsum",
+    description:
+      "Nunc ut tellus sed mauris iaculis rhoncus eget sit amet lacus.",
+    startDate: Date.now(),
+    duration: 11231,
+    keywords: ["demo"],
+    fundingRequired: 100,
+    fundingRaised: 0
+  },
+  {
+    id: 1,
+    title: "Suspendisse interdum",
+    description: "Nunc feugiat ipsum vitae ornare consectetur.",
+    startDate: Date.now(),
+    duration: 2344324,
+    keywords: ["demo"],
+    fundingRequired: 100,
+    fundingRaised: 30
+  },
+  {
+    id: 2,
+    title: "Cras aliquam",
+    description: "Nunc feugiat ipsum vitae ornare consectetur.",
+    startDate: Date.now(),
+    duration: 1231,
+    keywords: ["demo"],
+    fundingRequired: 101,
+    fundingRaised: 70
+  },
+  {
+    id: 3,
+    title: "Vestibulum aliquet",
+    description: "Morbi eleifend felis id molestie tempus.",
+    startDate: Date.now(),
+    duration: 23423234441,
+    keywords: ["demo"],
+    fundingRequired: 100,
+    fundingRaised: 100
+  }
+];
