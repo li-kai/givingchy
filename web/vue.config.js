d390dd252266becdc726e8d250d38536af328b11
Li Kai <falconets25601@gmail.com>
2018-03-13 21:58:42 +0800

    Set up nginx proxy

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8081',
-        pathRewrite: { '^/api': '' },
-        changeOrigin: true
-      }
-    }
+    public: `0.0.0.0:${process.env.HOST_PORT}`,
+    disableHostCheck: true
   }
 };
