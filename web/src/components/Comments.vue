1fb067eddad703358e29fcc9922b18782d8ceeff
Li Kai <falconets25601@gmail.com>
2018-03-15 20:37:31 +0800

    Add comments feature

---
+++ web/src/components/Comments.vue
@@ -0,0 +1,110 @@
+<template>
+  <section>
+    <h3>Comments</h3>
+    <ul v-if="comments.length > 0" class="comments">
+      <li v-for="(comment, index) in comments" :key="index" class="comments-item">
+        <p class="comments-p">{{comment.content}}</p>
+        <div class="comments-timestamp">posted {{comment.timeAgo}}</div>
+      </li>
+    </ul>
+    <p v-else class="comments-empty">
+      No comments yet. Get the ball rolling!
+    </p>
+    <el-form ref="form" :model="form" label-position="top" class="form"
+    >
+      <el-form-item label="Leave a comment">
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 2 }"
+          v-model="form.text">
+        </el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="submit" :disabled="!form.text.length">
+          Create
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
+
+export default {
+  name: 'comments',
+  props: {
+    projectId: {
+      required: true,
+    },
+  },
+  data() {
+    return {
+      comments: [],
+      form: {
+        text: '',
+      },
+    };
+  },
+  created() {
+    axios
+      .get(`/api/projects/${this.projectId}/comments`)
+      .then((res) => {
+        this.comments = res.data.map((comment) => ({
+          ...comment,
+          timeAgo: distanceInWordsToNow(comment.createdAt, { addSuffix: true }),
+        }));
+        console.log(res.data);
+      })
+      .catch((err) => console.error(err));
+  },
+  methods: {
+    submit() {
+      this.comments.push({
+        id: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
+        userId: 1,
+        projectId: this.projectId,
+        createdAt: Date.now(),
+        timeAgo: distanceInWordsToNow(Date.now(), { addSuffix: true }),
+        content: this.form.text,
+      });
+      axios
+        .post('/api/comments', {
+          userId: 1,
+          projectId: this.projectId,
+          content: this.form.text,
+        })
+        .then(() => {
+          // clear text input
+          this.form.text = '';
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.comments {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.comments-item {
+  padding-bottom: 0.25rem;
+}
+.comments-p {
+  margin-bottom: 0.15rem;
+}
+.comments-timestamp {
+  color: #909399;
+}
+.comments-empty {
+}
+.form {
+  margin-top: 1rem;
+}
+</style>
--- web/src/components/project/Project.vue
+++ web/src/components/project/Project.vue
@@ -2,10 +2,10 @@
   <el-row type="flex" justify="center">
     <el-col :xs="24" :sm="18">
     <el-row :gutter="20">
-      <el-col :xs="24" :sm="16">
+      <el-col :xs="24" :sm="16" :lg="10">
         <img src="https://via.placeholder.com/150x150" class="image">
       </el-col>
-      <el-col :xs="24" :sm="8">
+      <el-col :xs="24" :sm="8" :lg="14">
         <h1>{{project.title}}</h1>
         <h2>{{project.description}}</h2>
         <el-progress :percentage="fundingPercentage" :status="fundingStatus"></el-progress>
@@ -30,16 +30,27 @@
         </el-button>
       </el-col>
     </el-row>
+    <el-row class="keyline">
+      <el-col>
+        <comments :project-id="project.id"/>
+      </el-col>
+    </el-row>
     </el-col>
   </el-row>
 </template>
 
 <script>
+import Comments from '../Comments.vue';
 import axios from 'axios';
+import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
+import isFuture from 'date-fns/is_future';
 import { projects } from '../../fixtures';
 
 export default {
   name: 'project',
+  components: {
+    Comments,
+  },
   data() {
     return {
       project: projects[0],
@@ -48,16 +59,12 @@ export default {
   },
   computed: {
     timeLeft() {
-      const secondsLeft = (new Date(this.project.endTime) - Date.now()) / 1000
-      const minutesLeft = secondsLeft / 60;
-      if (minutesLeft < 60) return `Closes in ${Math.round(minutesLeft)} minutes`;
-      const hoursLeft = minutesLeft / 60;
-      if (hoursLeft < 24) return `Closes in ${Math.round(hoursLeft)} hours`;
-      const daysLeft = hoursLeft / 24;
-      if (daysLeft > 0) {
-        return `Closes in ${Math.round(daysLeft)} days`;
+      const { endTime } = this.project;
+      const ago = distanceInWordsToNow(this.project.endTime);
+      if (isFuture(endTime)) {
+        return `Closes in ${ago}`;
       }
-      return 'Project has ended';
+      return "Project has ended";
     },
     fundingPercentage() {
       const { amountRaised, amountRequired } = this.project;
@@ -97,5 +104,9 @@ export default {
 }
 .keywords {
   margin: 0.5rem 0.5rem 0 0;
+}
+.keyline {
+  margin-top: 2rem;
+  border-top: 1px solid #e4e7ed;
 }
 </style>
